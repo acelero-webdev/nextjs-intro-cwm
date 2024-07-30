da218a87c0d76e39d8276dc5c935ee05e8bbf115
Shane Kobylecky <skobylecky1@gmail.com>
2024-07-29 22:09:52 -0500

    Implement add user route.

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -3,8 +3,37 @@ import { NextRequest, NextResponse } from 'next/server';
 export function GET(request: NextRequest) {
     // Fetch users in db.
 
+    // Return data
     return NextResponse.json([
         { id: 1, name: 'Shane' },
         { id: 2, name: 'Mosh' },
     ]);
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    // Validate the body
+    // If invalid -> 404
+    if (!body.name) {
+        return NextResponse.json(
+            {
+                error: 'Name is required.',
+            },
+            {
+                status: 404,
+            }
+        );
+    }
+
+    // Else
+    return NextResponse.json(
+        {
+            id: 3,
+            name: body.name,
+        },
+        {
+            status: 201,
+        }
+    );
 }
