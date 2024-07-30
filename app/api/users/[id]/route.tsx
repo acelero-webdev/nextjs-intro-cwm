27a9201f17ca711aae05c4cce0a56ba7066369f7
Shane Kobylecky <skobylecky1@gmail.com>
2024-07-29 22:25:47 -0500

    Add a route for deleting a user.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -24,3 +24,58 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
         name: 'Shane',
     });
 }
+
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+    // Validate the request body
+    const body = await request.json();
+
+    // If invalid -> return 400 error
+    if (!body.name) {
+        return NextResponse.json(
+            {
+                error: 'You must provide a valid name for the updated user.',
+            },
+            {
+                status: 400,
+            }
+        );
+    }
+
+    // Check to see if users exists
+    // If invalid -> return 404 error
+    if (id < 0 || id > 10) {
+        return NextResponse.json(
+            {
+                error: 'User not found.',
+            },
+            {
+                status: 404,
+            }
+        );
+    }
+
+    // Update the database
+    return NextResponse.json({
+        id: id,
+        body: body.name,
+    });
+}
+
+export async function DELETE(request: NextRequest, { params: { id } }: Props) {
+    // Fetch users
+    // Check to see if the user exists.
+    if (id < 0 || id > 10) {
+        return NextResponse.json(
+            {
+                error: 'User not found.',
+            },
+            {
+                status: 404,
+            }
+        );
+    }
+
+    // Delete the users
+    // Return 200 response
+    return NextResponse.json({});
+}
